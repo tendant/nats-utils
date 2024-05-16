d4c0988c18f75e5e6d77462525b2c08c16fe46e9
Yangye Zhu <yangye.zhu@bryzi.io>
2024-05-16 16:24:23 -0700

    cmd demo: cleanup

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/tendant/nats-utils/processor"
-	"golang.org/x/exp/slog"
 )
 
 type ProcessorConfig struct {
@@ -20,26 +20,17 @@ func main() {
 	cleanenv.ReadEnv(&config)
 	nc, err := processor.CreateNc(config.NatsConfig)
 	defer nc.Drain()
-
-	slog.Info("natsurl", "NatsURL", config.NatsURL)
 	if err != nil {
-		slog.Error("Failed connecting to Nats Server", "NatsURL", config.NatsURL, "err", err)
 		os.Exit(-1)
-	} else {
-		slog.Info("Connected to Nats Server!")
 	}
 
 	js, err := processor.CreateJS(nc)
 	if err != nil {
-		slog.Error("Failed Connect to Stream Context! ", "err", err)
 		os.Exit(-1)
-	} else {
-		slog.Info("Connect to Stream Context!")
 	}
 
 	consumer, err := processor.CreateOrUpdateConsumer(context.Background(), js, config.ConsumerConfig)
 	if err != nil {
-		slog.Error("Failed CreateOrUpdateConsumer", "err", err)
 		os.Exit(-1)
 	}
 
