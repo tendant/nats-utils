b204136dd0c349673d905ad61ef389360445849e
Yangye Zhu <yangye.zhu@bryzi.io>
2025-06-06 13:51:36 -0700

    processor config: new fields for Consumer

--- processor/config.go
+++ processor/config.go
@@ -15,4 +15,7 @@ type ConsumerConfig struct {
 	FilterSubjects string `env:"FILTER_SUBJECTS" env-default:"demo.sub1,demo.sub2"`
 	DeliverPolicy  string `env:"DELIVER_POLICY" env-default:"new"`
 	StreamName     string `env:"STREAM_NAME" env-default:"DEMO"`
+	MaxDeliver     int    `env:"MAX_DELIVER" env-default:"5"`
+	MaxAckPending  int    `env:"MAX_ACK_PENDING" env-default:"1000"`
+	AckWait        string `env:"ACK_WAIT" env-default:"PT30S"`
 }
